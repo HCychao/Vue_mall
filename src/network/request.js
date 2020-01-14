e5c31df66d9c64713d0555aaf3c1692a72e33d4e
HCychao <1219672433@qq.com>
2020-01-14 16:28:29 +0800

    '首页'

---
+++ src/network/request.js
@@ -0,0 +1,26 @@
+import axios from 'axios'
+
+export function request(config) {
+  //创建axios实例
+  const instance = axios.create({
+    baseURL: 'http://123.207.32.32:8000/api/hy',
+    timeout: 5000
+  })
+
+  //axios的拦截器
+  //请求拦截器的作用
+  instance.interceptors.request.use(config => {
+    return config;
+  }, err => {
+  })
+
+  //响应拦截
+  instance.interceptors.response.use(res => {
+    return res.data
+  }, err => {
+    console.log(err);
+  })
+
+  //发送网络请求
+  return instance(config)
+}
