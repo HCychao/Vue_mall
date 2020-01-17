e8b235696239d681fea789fbf99f53b3ab43035d
HCychao <1219672433@qq.com>
2020-01-17 15:38:32 +0800

    '首页'

---
+++ src/views/detail/Detail.vue
@@ -0,0 +1,88 @@
+<template>
+  <div id="detail">
+    <detail-nav-bar class="detail-nav"/>
+    <scroll class="content" ref="scroll">
+      <detail-swiper :top-images="topImages" />
+      <detail-base-info :goods="goods"/>
+      <detail-shop-info :shop="shop"/>
+      <detail-goods-info :detail-info="detailInfo" @imageLoad="imageLoad"/>
+      <detail-param-info :param-info="paramInfo"/>
+    </scroll>
+  </div>
+</template>
+
+<script>
+  import Scroll from 'components/common/scroll/Scroll'
+
+  import DetailNavBar from './childComps/DetailNavBar'
+  import DetailSwiper from './childComps/DetailSwiper'
+  import DetailBaseInfo from './childComps/DetailBaseInfo'
+  import DetailShopInfo from './childComps/DetailShopInfo'
+  import DetailGoodsInfo from './childComps/DetailGoodsInfo'
+  import DetailParamInfo from './childComps/DetailParamInfo'
+
+  import {getDetail, Goods, Shop, GoodsParam} from "network/detail";
+
+  export default {
+		name: "Detail",
+    components: {
+		  Scroll,
+		  DetailNavBar,
+      DetailSwiper,
+      DetailBaseInfo,
+      DetailShopInfo,
+      DetailGoodsInfo,
+      DetailParamInfo
+    },
+    data(){
+		  return {
+		    iid: null,
+        topImages: [],
+        goods: {},
+        shop: {},
+        detailInfo: {},
+        paramInfo: {},
+      }
+    },
+    created() {
+		  //保存传入的iid
+		  this.iid = this.$route.params.iid;
+		  //根据iid请求数据
+      getDetail(this.iid).then(res => {
+        const data = res.result;
+        //获取顶部的图片轮播数据
+        this.topImages = data.itemInfo.topImages;
+        //获取商品信息
+        this.goods = new Goods(data.itemInfo , data.columns , data.shopInfo.services);
+        //获取店铺信息
+        this.shop = new Shop(data.shopInfo);
+        //获取详情数据
+        this.detailInfo = data.detailInfo;
+        //获取参数信息
+        this.paramInfo = new GoodsParam(data.itemParams.info , data.itemParams.rule);
+      })
+    },
+    methods: {
+		  imageLoad(){
+		    this.$refs.scroll.refresh();
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  #detail{
+    position: relative;
+    z-index: 9;
+    background: #ffffff;
+    height: 100vh;
+  }
+  .detail-nav{
+    position: relative;
+    z-index: 9;
+    background: #ffffff;
+  }
+  .content{
+    height: calc(100% - 44px);
+  }
+</style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,44 +1,179 @@
 <template>
   <div id="home">
     <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
-    <home-swiper :banners="banners"></home-swiper>
-    <recommend-view :recommends="recommends"></recommend-view>
+    <tab-control class="tab-control"
+                 :titles="['流行', '新款', '精选']"
+                 @tabClick="tabClick"
+                 ref="tabControl1" v-show="isTabFixed"/>
+    <scroll class="content"
+            ref="scroll"
+            :probe-type="3"
+            @scroll="contentScroll"
+            :pull-up-load="true"
+            @pullingUp="loadMore">
+      <home-swiper :banners="banners" @swiperImageLoad="swiperImageLoad" />
+      <recommend-view :recommends="recommends"/>
+      <feature-view/>
+      <tab-control :titles="['流行', '新款', '精选']" @tabClick="tabClick" ref="tabControl2"/>
+      <good-list :goods="showGoods"/>
+    </scroll>
+    <back-top @click.native="backClick" v-show="isShowBackTop"/>
   </div>
 </template>
 
 <script>
   import NavBar from 'components/common/navbar/NavBar'
-  import {getHomeMultidata} from 'network/home'
+  import Scroll from 'components/common/scroll/Scroll'
+  import TabControl from 'components/content/tabControl/TabControl'
+  import BackTop from 'components/content/backTop/BackTop'
+  import GoodList from 'components/content/goods/GoodsList'
+
   import HomeSwiper from './childComps/HomeSwiper'
   import RecommendView from './childComps/RecommendView'
+  import FeatureView from './childComps/FeatureView'
+
+  import {getHomeMultidata , getHomeGoods } from 'network/home'
+  import {debounce} from "common/utils";
 
   export default {
     name: "Home",
     components: {
       NavBar,
+      Scroll,
+      TabControl,
+      BackTop,
       HomeSwiper,
-      RecommendView
+      RecommendView,
+      FeatureView,
+      GoodList
     },
     data(){
       return {
         banners: [],
         recommends: [],
+        goods: {
+          'pop': {page: 0 , list: [] },
+          'new': {page: 0 , list: [] },
+          'sell': {page: 0 , list: [] }
+        },
+        currentType: 'pop',
+        isShowBackTop: false,
+        tabOffsetTop: 0,
+        isTabFixed: false,
+        saveY: 0
+      }
+    },
+    computed: {
+      showGoods(){
+        return this.goods[this.currentType].list;
+      },
+      activated(){
+        this.$refs.scroll.scrollTo(0 , -this.saveY , 0);
+        this.$refs.scroll.refresh();
+      },
+      deactivated(){
+        this.saveY = this.$refs.scroll.getScrollY();
       }
     },
     created() {
       //请求多个数据
-      getHomeMultidata().then(res => {
-        console.log(res);
-        this.banners = res.data.banner.list;
-        this.recommends = res.data.recommend.list;
+      this.getHomeMultidata(),
+
+      //请求商品数据
+      this.getHomeGoods('pop')
+      this.getHomeGoods('new')
+      this.getHomeGoods('sell')
+
+    },
+    mounted() {
+      //监听item中图片加载完成
+      const refresh = debounce(this.$refs.scroll.refresh , 500);
+      this.$bus.$on('itemImageLoad' , () => {
+        refresh();
       })
+    },
+    methods: {
+      /**
+       * 网络请求的方法
+       */
+      getHomeMultidata(){
+        getHomeMultidata().then(res => {
+          this.banners = res.data.banner.list;
+          this.recommends = res.data.recommend.list;
+        })
+      },
+      getHomeGoods(type){
+        const page = this.goods[type].page + 1;
+        getHomeGoods(type , page).then(res => {
+          this.goods[type].list.push(...res.data.list);
+          this.goods[type].page += 1;
+          //上拉加载更多
+          this.$refs.scroll.finishPullUp();
+        })
+      },
+      /**
+       * 事件监听的方法
+       */
+      tabClick(index){
+        switch (index) {
+          case 0:
+            this.currentType = 'pop';
+            break;
+          case 1:
+            this.currentType = 'new';
+            break;
+          case 2:
+            this.currentType = 'sell';
+            break;
+        }
+        this.$refs.tabControl1.currentIndex = index;
+        this.$refs.tabControl2.currentIndex = index;
+      },
+      backClick(){
+        this.$refs.scroll.scrollTo(0,0,500);
+      },
+      contentScroll(position) {
+        //判断BackTop是否显示
+        this.isShowBackTop = (-position.y) > 1000
+        //判断tabControl是否吸顶（position: fixed）
+        this.isTabFixed = (-position.y) > this.tabOffsetTop;
+      },
+      loadMore(){
+        this.getHomeGoods(this.currentType);
+      },
+      swiperImageLoad(){
+        this.tabOffsetTop = this.$refs.tabControl2.$el.offsetTop;
+      }
     }
   }
 </script>
 
 <style scoped>
+  #home{
+    /*padding-top: 44px;*/
+    height: 100vh;
+    position: relative;
+  }
   .home-nav{
     background-color: var(--color-tint);
     color: white;
+    /*position: fixed;
+    left: 0;
+    right: 0;
+    top: 0;
+    z-index: 9;*/
+  }
+  .content {
+    overflow: hidden;
+    position: absolute;
+    top: 44px;
+    bottom: 49px;
+    left: 0;
+    right: 0;
+  }
+  .tab-control{
+    position: relative;
+    z-index: 9;
+    background: rgba(255,255,255,.8);
   }
 </style>
